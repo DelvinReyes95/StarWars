1b3c42658b03ba3c1024ba1cad29b76c7f2dd24a
DelvinReyes95 <101748470+DelvinReyes95@users.noreply.github.com>
2022-09-23 17:38:59 -0400

    Intro page (#7)
    * implemented css with functionality
    
    * implemented stars in random position
    
    * updated intro page
    
    * fixed functionality

---
+++ intro.js
@@ -0,0 +1,20 @@
+const stars = 500 //Here is where the size of stars can be modified
+
+// This will generate random stars on the page
+for (let i =0; i < stars; i++) {
+    let star = document.createElement("div")
+    star.className = 'stars'
+    const xy = randomPosition();
+    star.style.top = xy[0] + 'px'
+    star.style.left = xy[1] + 'px'
+    document.body.append(star)
+}
+
+//Allow stars to appear randomly on screen
+function randomPosition() {
+    const y = window.innerWidth
+    const x = window.innerHeight
+    const randomX = Math.floor(Math.random() * x)
+    const randomY = Math.floor(Math.random() * y)
+    return [randomX, randomY]
+}
